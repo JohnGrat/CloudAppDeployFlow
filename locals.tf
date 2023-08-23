962be6848adf48528bdbf28df0fabd5283d936a6
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-08-23 15:41:31 +0200

    added terraform resourcegroup and local

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  RGname = azurerm_resource_group.RG-TerraformLABB2-JG.name
+  RGlocation = azurerm_resource_group.RG-TerraformLABB2-JG.location
+}
---
+++ resourcegroup.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "RG-TerraformLABB2-JG" {
+    name = "RG-TerraformLABB2-JG"
+    location = "North Europe"
+}
