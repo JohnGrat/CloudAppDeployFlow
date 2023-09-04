97ce151b6d8bcf4af811c4eb13036f1be5a0b202
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-09-04 18:48:54 +0200

    connection string test

--- CloudApp/CloudApp/Program.cs
+++ CloudApp/CloudApp/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Register the WeatherRepository using the desired configuration
-//string cosmosConnectionString = builder.Configuration.GetConnectionString("Database");
-string cosmosConnectionString = "LOL";
+
+string cosmosConnectionString = builder.Configuration.GetConnectionString("Database");
 string databaseName = "ToDoList";
 string containerName = "Items";
 builder.Services.AddSingleton<WeatherRepository>(sp =>
